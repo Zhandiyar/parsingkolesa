9ed84a202cb603da295161235fc4230f16e100de
Zhandiyar <zhandik.1997@mail.ru>
2023-11-15 23:11:23 +0300

    added script create car_description_table

---
+++ src/main/resources/db/migration/V2__create_car_description_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE car_description (
+     id BIGSERIAL PRIMARY KEY,
+     car_id BIGINT,
+     generation varchar(255) NOT NULL,
+     body varchar(255) NOT NULL,
+     engine_volume varchar(255) NOT NULL,
+     transmission varchar(255) NOT NULL,
+     drive varchar(255) NOT NULL,
+     rudder varchar(255) NOT NULL,
+     color varchar(255) NOT NULL,
+     customs_cleared varchar(255) NOT NULL,
+     description TEXT,
+     seller_comment TEXT,
+     FOREIGN KEY (car_id) REFERENCES car(id) ON DELETE CASCADE
+);
